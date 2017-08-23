b3a42fb6fb3a66955453fca45ceb79e092d8c5a6
Brett Schules <brettschules@gmail.com>
2017-08-23 17:18:49 -0400

    Been a while since update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 # #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 # #   Character.create(name: 'Luke', movie: movies.first)
 # Stock.create(name: "Coca-Cola", symbol: "KO", price_purchased: 45.75, units: 12, status: "Excuted", order:"Buy", user_id: 1)
-# # User.create(name: "brett", username: "Brett", password: "Brett",password_confirmation: "Brett", account_balance: 10000.00, image: "")
+# User.create(name: "brett", username: "Brett", password: "Brett",password_confirmation: "Brett", account_balance: 100000.00, image: "")
 # Stock.create(name: "Verzion", symbol: "VZ", price_purchased: 50.75, units: 4, status: "Excuted", order:"Buy", user_id: 1)
 # Stock.create(name: "Amerigas", symbol: "APU", price_purchased: 47.70, units: 7, status: "Excuted", order:"Buy", user_id: 1)
